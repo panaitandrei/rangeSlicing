2bfb5881c90f24fb6ccc7803bd021b22861a5cb1
apanait <pnt_andrei@yahoo.com>
2019-07-13 17:04:22 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setuptools.setup(
      scripts=['rangeSlicing'] ,
      author="Andrei Panait",
      author_email="pnt_andrei@yahoo.com",
-     description="A Docker and AWS utility package",
+     description="A range splitting package to divide a longer range in smaller batches.",
      long_description=long_description,
    long_description_content_type="text/markdown",
      url="https://github.com/panaitandrei/rangeSlicing",
