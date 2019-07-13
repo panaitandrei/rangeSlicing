db8d43860c0dc9fc2e9714f3f09431334f8dfe3f
apanait <pnt_andrei@yahoo.com>
2019-07-13 17:01:38 +0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+import setuptools
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+setuptools.setup(
+     name='rangeSlicing',
+     version='0.1',
+     scripts=['rangeSlicing'] ,
+     author="Andrei Panait",
+     author_email="pnt_andrei@yahoo.com",
+     description="A Docker and AWS utility package",
+     long_description=long_description,
+   long_description_content_type="text/markdown",
+     url="https://github.com/panaitandrei/rangeSlicing",
+     packages=setuptools.find_packages(),
+     classifiers=[
+         "Programming Language :: Python :: 3",
+         "License :: OSI Approved :: Apache License",
+         "Operating System :: OS Independent",
+     ],
+ )
